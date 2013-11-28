95b76a2c73b0f37e95cbacfe48bffc1c0fec2f23
kenvinny <kenvinny@163.com>
2013-11-28 15:50:41 +0800

    Initial Commit

---
+++ FirstTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FirstTest
+//
+//  Created by ken on 13-11-28.
+//  Copyright (c) 2013年 ken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FirstTestTests/FirstTestTests.m
@@ -0,0 +1,34 @@
+//
+//  FirstTestTests.m
+//  FirstTestTests
+//
+//  Created by ken on 13-11-28.
+//  Copyright (c) 2013年 ken. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FirstTestTests : XCTestCase
+
+@end
+
+@implementation FirstTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
